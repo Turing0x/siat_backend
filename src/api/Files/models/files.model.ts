50406f85822d87a69d31aec2c1bf5202cfc5b9f6
Turing0x <rauligf97@gmail.com>
2024-10-05 18:41:06 -0400

    confing files managment doned

---
+++ src/api/Files/models/files.model.ts
@@ -0,0 +1,5 @@
+export type ConfigFiles = {
+    file_name: string;
+    full_path: string;
+    content: string;
+}
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { UserRouter } from '../api/User/users.routes';
 import { ExerciseRouter } from '../api/Exercise/exercises.routes';
 import { SolutionRouter } from '../api/Solution/solutions.routes';
+import { FilesRouter } from '../api/Files/files.routes';
 
 export const api = express.Router();
 
@@ -10,3 +11,5 @@ api.use("/users", UserRouter);
 api.use("/exercises", ExerciseRouter);
 
 api.use("/solutions", SolutionRouter);
+
+api.use("/files", FilesRouter);
